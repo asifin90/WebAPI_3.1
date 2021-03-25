d55ba62def4a3f81df1b3159340712f5c4767ce8
Asif Sayyad <asifin90@gmail.com>
2021-03-25 12:23:16 +0530

    Add files via upload

---
+++ API/ConsumingWebApi/ConsumingWebAPI/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using ConsumingWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ConsumingWebAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IHttpClientFactory _clientFactory;
+
+        public HomeController(ILogger<HomeController> logger, IHttpClientFactory clientFactory)
+        {
+            _logger = logger;
+            _clientFactory = clientFactory;
+        }
+
+        public async Task<IActionResult> LoadData()
+        {
+            IEnumerable<WeatherForecast> data;
+            var request = new HttpRequestMessage(HttpMethod.Get, "http://localhost:18039/weatherforecast");
+            var client = _clientFactory.CreateClient();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var response = await client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseStream = await response.Content.ReadAsStreamAsync();
+                data = await JsonSerializer.DeserializeAsync<IEnumerable<WeatherForecast>>(responseStream);
+            }
+            else
+            {
+                data = new List<WeatherForecast>();
+            }
+            return View(data);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
